3d312454b6a304c67f17926b057a643991a892f8
Pawne Webfreak <pawn.webfreaksolution@gmail.com>
2023-05-02 15:30:27 +0530

    new version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Country Pickers Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(useMaterial3: true),
       routes: {
         '/': (context) => DemoPage(),
       },
